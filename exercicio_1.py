9d410fa7766a08309f120a584299f5824c7d9b3e
brunofafabi <163673577+brunofafabi@users.noreply.github.com>
2024-03-31 22:06:17 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -17,3 +17,9 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+número = int(input('Me diga um número qualquer: '))
+resultado = número % 2
+if resultado == 0:
+  print('O número {} é PAR'.format(número))
+else:
+  print('O número {} é ÍMPAR'.format(número))
